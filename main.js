6b1bfac91ad8977669347c4e5320f83b1f58361e
Sam Dawes <sdawes@usc.edu>
2019-03-19 18:45:29 -0700

    changed url

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let connection = new WebSocket('ws://localhost:8080');
+//let connection = new WebSocket('ws://localhost:8080');
+let connection = new WebSocket('https://sdawes-websockets.herokuapp.com');
 
 connection.onopen = () => {
   console.log('connected from the frontend');
